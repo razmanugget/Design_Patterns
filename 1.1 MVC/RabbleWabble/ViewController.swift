0a8d67e925d31b8cb596c35ff89e4d1f3d718459
Rami <rami_s@hotmail.com>
2020-02-10 14:31:58 +0100

    1.1 MVC initial commit

---
+++ 1.1 MVC/RabbleWabble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RabbleWabble
+//
+//  Created by Rami on 2/10/20.
+//  Copyright © 2020 Lyfebug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
