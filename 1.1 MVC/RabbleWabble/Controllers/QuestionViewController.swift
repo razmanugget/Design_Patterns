715e57b24bd8285dfcc319f26a51f3bddeb427fe
Rami <rami_s@hotmail.com>
2020-02-12 12:36:14 +0100

    3 - handle correct / incorrect counts

--- 1.1 MVC/RabbleWabble/Controllers/QuestionViewController.swift
+++ 1.1 MVC/RabbleWabble/Controllers/QuestionViewController.swift
@@ -23,6 +23,38 @@ public class QuestionViewController: UIViewController {
    }
    
    
+   // MARK: - Actions
+   @IBAction func toggleAnswerLabels(_ sender: Any) {
+      questionView.answerLabel.isHidden = !questionView.answerLabel.isHidden
+      questionView.hintLabel.isHidden = !questionView.hintLabel.isHidden
+   }
+   
+   @IBAction func handleCorrect(_ sender: Any) {
+      correctCount += 1
+      questionView.correctCountLabel.text = "\(correctCount)"
+      showNextQuestion()
+   }
+   
+   @IBAction func handleIncorrect(_ sender: Any) {
+      incorrectCount += 1
+      questionView.incorrectCountLabel.text = "\(incorrectCount)"
+      showNextQuestion()
+   }
+   
+   
+   private func showNextQuestion() {
+      questionIndex += 1
+      guard questionIndex < questionGroup.questions.count else {
+         // TODO: - handle this
+         print("study over")
+         if correctCount > incorrectCount {
+            print("awesome")
+         }
+         return
+      }
+      showQuestion()
+   }
+   
    // MARK: - VC Life Cycle
    public override func viewDidLoad() {
       
