ecc6bcc090c29658de5e630eea8ec37908962865
Rami <rami_s@hotmail.com>
2020-02-12 12:21:29 +0100

    3 - questions hooked up

--- 1.1 MVC/RabbleWabble/Controllers/QuestionViewController.swift
+++ 1.1 MVC/RabbleWabble/Controllers/QuestionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  QuestionViewController.swift
 //  RabbleWabble
 //
 //  Created by Rami on 2/10/20.
@@ -9,7 +9,36 @@
 import UIKit
 
 public class QuestionViewController: UIViewController {
-
- 
+   
+   // MARK: - Properties
+   public var questionGroup = QuestionGroup.basicPhrases()
+   public var questionIndex = 0
+   
+   public var correctCount = 0
+   public var incorrectCount = 0
+   
+   public var questionView: QuestionView! {
+      guard isViewLoaded else { return nil }
+      return (view as! QuestionView)
+   }
+   
+   
+   // MARK: - VC Life Cycle
+   public override func viewDidLoad() {
+      
+      super.viewDidLoad()
+      showQuestion()
+   }
+   
+   private func showQuestion() {
+      let question = questionGroup.questions[questionIndex]
+      
+      questionView.answerLabel.text = question.answer
+      questionView.promptLabel.text = question.prompt
+      questionView.hintLabel.text = question.hint
+      
+      questionView.answerLabel.isHidden = true
+      questionView.hintLabel.isHidden = true
+   }
+   
 }
-
