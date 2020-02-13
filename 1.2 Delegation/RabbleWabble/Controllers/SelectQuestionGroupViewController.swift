4e026f21d9ec9c9317f05104f9690620ec5d73e8
Rami <rami_s@hotmail.com>
2020-02-13 21:07:00 +0100

    4 - displaying a list of table groups

--- 1.2 Delegation/RabbleWabble/Controllers/SelectQuestionGroupViewController.swift
+++ 1.2 Delegation/RabbleWabble/Controllers/SelectQuestionGroupViewController.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2020 Lyfebug. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class SelectQuestionGroupViewController: UIViewController {
+   
+   // MARK: - Outlets
+   @IBOutlet internal var tableView: UITableView! {
+      didSet {
+         tableView.tableFooterView = UIView()
+      }
+   }
+   
+   // MARK: - Properties
+   public let questionGroups = QuestionGroup.allGroups()
+   private var selectedQuestionGroup: QuestionGroup!
+   
+   
+//   (_ tableView: UITableView, numberOfRowsInSection section: Int)
+//   -> Int {
+//   return questionGroups.count
+//   }
+//
+//   public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+//      -> UITableViewCell {
+//         return UITableViewCell()
+//   }
+}
