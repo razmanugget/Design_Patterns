aaff7e51a214ac76c4793c41c2364deb19515fa9
Rami <rami_s@hotmail.com>
2020-02-17 11:41:20 +0100

    5 sequential strategy added

---
+++ 1.3 Strategy/RabbleWabble/Strategies/SequentialQuestionStrategy.swift
@@ -0,0 +1,51 @@
+//
+//  SequentialQuestionStrategy.swift
+//  RabbleWabble
+//
+//  Created by Rami on 2/17/20.
+//  Copyright © 2020 Lyfebug. All rights reserved.
+//
+
+import Foundation
+
+public class SequentialQuestionStrategy: QuestionStrategy {
+   // MARK: - Properties
+   public var correctCount: Int = 0
+   public var incorrectCount: Int = 0
+   private let questionGroup: QuestionGroup
+   private var questionIndex = 0
+   
+   // MARK: - Object Lifecycle
+   public init(questionGroup: QuestionGroup) {
+      self.questionGroup = questionGroup
+   }
+   
+   // MARK: - QuestionStrategy
+   public var title: String {
+      return questionGroup.title
+   }
+   
+   public func currentQuestion() -> Question {
+      return questionGroup.questions[questionIndex]
+   }
+   
+   public func advanceToNextQuestion() -> Bool {
+      guard questionIndex + 1 < questionGroup.questions.count else {
+         return false
+      }
+      questionIndex += 1
+      return true
+    }
+     
+    public func markQuestionCorrect(_ question: Question) {
+       correctCount += 1
+    }
+    
+    public func markQuestionIncorrect(_ question: Question) {
+       incorrectCount += 1
+    }
+    
+    public func questionIndexTitle() -> String {
+       return "\(questionIndex + 1)/" + "\(questionGroup.questions.count)"
+    }
+}
