91e3f8213d8672b1fd71e49b48a59a0a090c7850
Rami <rami_s@hotmail.com>
2020-02-19 11:29:23 +0100

    6 - added delegate/dataSource

--- 1.4 Singleton/RabbleWabble/Controllers/AppSettingsViewController.swift
+++ 1.4 Singleton/RabbleWabble/Controllers/AppSettingsViewController.swift
@@ -20,3 +20,41 @@ public class AppSettingsViewController: UITableViewController {
       tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
    }
 }
+
+
+// MARK: - UITableView DataSource
+extension AppSettingsViewController {
+   public override func tableView(_ tableView: UITableView,
+                                  numberOfRowsInSection section: Int)
+      -> Int {
+         return QuestionStrategyType.allCases.count
+   }
+   
+   public override func tableView(_ tableView: UITableView,
+                                  cellForRowAt indexPath: IndexPath)
+      -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier,
+                                                  for: indexPath)
+         let questionStrategyType = QuestionStrategyType.allCases[indexPath.row]
+         
+         cell.textLabel?.text = questionStrategyType.title()
+         
+         if appSettings.questionStrategyType == questionStrategyType {
+            cell.accessoryType = .checkmark
+         } else {
+            cell.accessoryType = .none
+         }
+         return cell
+   }
+}
+
+
+// MARK: - UITableView Delegate
+extension AppSettingsViewController {
+   public override func tableView(_ tableView: UITableView,
+                                  didSelectRowAt indexPath: IndexPath) {
+      let questionStrategyType = QuestionStrategyType.allCases[indexPath.row]
+      appSettings.questionStrategyType = questionStrategyType
+      tableView.reloadData()
+   }
+}
