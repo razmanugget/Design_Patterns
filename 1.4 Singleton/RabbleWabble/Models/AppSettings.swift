8bc928da1d3d2800ee93e8ff593dcd0337886688
Rami <rami_s@hotmail.com>
2020-02-19 10:28:12 +0100

    6 - setting up strategy choice

--- 1.4 Singleton/RabbleWabble/Models/AppSettings.swift
+++ 1.4 Singleton/RabbleWabble/Models/AppSettings.swift
@@ -9,6 +9,11 @@
 import Foundation
 
 public class AppSettings {
+   // MARK: - Keys
+   private struct Keys {
+      static let questionStrategy = "questionStrategy"
+   }
+   
    // MARK: - Static Properties
    public static let shared = AppSettings()
    
@@ -18,4 +23,28 @@ public class AppSettings {
 
 
 // MARK: - QuestionStrategyType
-
+public enum QuestionStrategyType: Int, CaseIterable {
+   case random
+   case sequential
+   
+   // MARK: - Instance Methods
+   public func title() -> String {
+      switch self {
+      case .random:
+         return "Random"
+      case . sequential:
+         return "Sequential"
+      }
+   }
+   
+   public func questionStrategy(for questionGroup: QuestionGroup)
+      -> QuestionStrategy {
+         switch self {
+         case .random:
+            return RandomQuestionStrategy(questionGroup: questionGroup)
+         case .sequential:
+            return SequentialQuestionStrategy(questionGroup: questionGroup)
+         }
+   }
+   
+}
