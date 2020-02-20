91a7966078b78a51363239c448055f8b67ed2459
Rami <rami_s@hotmail.com>
2020-02-20 17:03:25 +0100

    7 - added retrieve to DiskCaretaker codable

--- 1.5 Memento/RabbleWabble/Caretakers/DiskCaretaker.swift
+++ 1.5 Memento/RabbleWabble/Caretakers/DiskCaretaker.swift
@@ -12,13 +12,32 @@ public final class DiskCaretaker {
    public static let decoder = JSONDecoder()
    public static let encoder = JSONEncoder()
    
+   // generic method declared that conforms any object to codable
    public static func save<T: Codable>(_ object: T, to fileName: String) throws {
       do {
          let url = createDocumentURL(withFileName: fileName)
          let data = try encoder.encode(object)
+         // creates a temp file then moves it to the desired path
+         // performance hit, but ensures data is never corrupted
          try data.write(to: url, options: .atomic)
       } catch (let error) {
          print("Save failed: Object: '\(object)', " + "Error: '\(error)'")
+         throw error
+      }
+   }
+   
+   public static func retrieve<T: Codable>(_ type: T.Type, from fileName: String) throws -> T {
+      let url = createDocumentURL(withFileName: fileName)
+      return try retrieve(T.self, from: url)
+   }
+   
+   public static func retrieve<T: Codable>(_ type: T.Type, from url: URL) throws
+      -> T {
+      do {
+         let data = try Data(contentsOf: url)
+         return try decoder.decode(T.self, from: data)
+      } catch (let error) {
+         print("Retrieve failed: URL: '\(url)', Error: '\(error)'")
          throw error
       }
    }
