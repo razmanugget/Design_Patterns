0e7603f2aab317fa4c9f8f184ba33cd6fabaa903
Rami <rami_s@hotmail.com>
2020-02-20 16:47:31 +0100

    7 - Question conforms to codable, caretaker stub added

---
+++ 1.5 Memento/RabbleWabble/Caretakers/DiskCaretaker.swift
@@ -0,0 +1,14 @@
+//
+//  DiskCaretaker.swift
+//  RabbleWabble
+//
+//  Created by Rami on 2/20/20.
+//  Copyright © 2020 Lyfebug. All rights reserved.
+//
+
+import Foundation
+
+public final class DiskCaretaker {
+   public static let decoder = JSONDecoder()
+   public static let encoder = JSONEncoder()
+}
--- 1.5 Memento/RabbleWabble/Models/Question.swift
+++ 1.5 Memento/RabbleWabble/Models/Question.swift
@@ -8,9 +8,15 @@
 
 import Foundation
 
-public struct Question {
+public class Question: Codable {
    public let answer: String
    public let hint: String?
    public let prompt: String
+   
+   public init(answer: String, hint: String?, prompt: String) {
+      self.answer = answer
+      self.hint = hint
+      self.prompt = prompt
+   }
 }
 
