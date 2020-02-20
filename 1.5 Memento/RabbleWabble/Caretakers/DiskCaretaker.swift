94662b5b757d7a3e7f9edfec0fb1b4251eb0f0d1
Rami <rami_s@hotmail.com>
2020-02-20 16:55:30 +0100

    7 - diskCaretaker created

--- 1.5 Memento/RabbleWabble/Caretakers/DiskCaretaker.swift
+++ 1.5 Memento/RabbleWabble/Caretakers/DiskCaretaker.swift
@@ -11,4 +11,24 @@ import Foundation
 public final class DiskCaretaker {
    public static let decoder = JSONDecoder()
    public static let encoder = JSONEncoder()
+   
+   public static func save<T: Codable>(_ object: T, to fileName: String) throws {
+      do {
+         let url = createDocumentURL(withFileName: fileName)
+         let data = try encoder.encode(object)
+         try data.write(to: url, options: .atomic)
+      } catch (let error) {
+         print("Save failed: Object: '\(object)', " + "Error: '\(error)'")
+         throw error
+      }
+   }
+   
+   public static func createDocumentURL(
+      withFileName fileName: String) -> URL {
+      let fileManager = FileManager.default
+      let url = fileManager.urls(for: .documentDirectory,
+                                 in: .userDomainMask).first!
+      return url.appendingPathComponent(fileName)
+         .appendingPathExtension("json")
+   }
 }
