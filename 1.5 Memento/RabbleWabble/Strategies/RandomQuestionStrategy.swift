60cc45447d3f08e547a02812e943062bbdb95d77
Rami <rami_s@hotmail.com>
2020-02-21 17:40:28 +0100

    Make both Random & Sequential QS subclasses of BaseQS ch 7

--- 1.5 Memento/RabbleWabble/Strategies/RandomQuestionStrategy.swift
+++ 1.5 Memento/RabbleWabble/Strategies/RandomQuestionStrategy.swift
@@ -9,49 +9,15 @@
 import Foundation
 import GameplayKit.GKRandomSource      // singleton import
 
-public class RandomQuestionStrategy: QuestionStrategy {
-   // MARK: - Properties
-   public var correctCount: Int = 0
-   public var incorrectCount: Int = 0
-   private let questionGroup: QuestionGroup
-   private var questionIndex = 0
-   private let questions: [Question]
+public class RandomQuestionStrategy: BaseQuestionStrategy {
    
-   // MARK: - Object Lifecycle
-   public init(questionGroup: QuestionGroup) {
-      self.questionGroup = questionGroup
-      
+   public convenience init(
+      questionGroupCaretaker: QuestionGroupCaretaker) {
+      let questionGroup = questionGroupCaretaker.selectedQuestionGroup!
       let randomSource = GKRandomSource.sharedRandom()
-      self.questions = randomSource.arrayByShufflingObjects(
+      let questions = randomSource.arrayByShufflingObjects(
          in: questionGroup.questions) as! [Question]
-   }
-   
-   // MARK: - QuestionStrategy
-   public var title: String {
-      return questionGroup.title
-   }
-   
-   public func currentQuestion() -> Question {
-      return questions[questionIndex]
-   }
-   
-   public func advanceToNextQuestion() -> Bool {
-     guard questionIndex + 1 < questionGroup.questions.count else {
-        return false
-     }
-     questionIndex += 1
-     return true
-   }
-    
-   public func markQuestionCorrect(_ question: Question) {
-      correctCount += 1
-   }
-   
-   public func markQuestionIncorrect(_ question: Question) {
-      incorrectCount += 1
-   }
-   
-   public func questionIndexTitle() -> String {
-      return "\(questionIndex + 1)/" + "\(questionGroup.questions.count)"
+      self.init(questionGroupCaretaker: questionGroupCaretaker,
+                questions: questions)
    }
 }
--- 1.5 Memento/RabbleWabble/Strategies/SequentialQuestionStrategy.swift
+++ 1.5 Memento/RabbleWabble/Strategies/SequentialQuestionStrategy.swift
@@ -8,50 +8,13 @@
 
 import Foundation
 
-public class SequentialQuestionStrategy: QuestionStrategy {
-   // MARK: - Properties
-   public var correctCount: Int {
-      get { return questionGroup.score.correctCount }
-      set { questionGroup.score.correctCount = newValue }
-   }
-   public var incorrectCount: Int {
-      get { return questionGroup.score.incorrectCount }
-      set { questionGroup.score.incorrectCount = newValue }
-   }
-   private let questionGroup: QuestionGroup
-   private var questionIndex = 0
+public class SequentialQuestionStrategy: BaseQuestionStrategy {
    
-   // MARK: - Object Lifecycle
-   public init(questionGroup: QuestionGroup) {
-      self.questionGroup = questionGroup
+   public convenience init(
+      questionGroupCaretaker: QuestionGroupCaretaker) {
+      let questionGroup = questionGroupCaretaker.selectedQuestionGroup!
+      let questions = questionGroup.questions
+      self.init(questionGroupCaretaker: questionGroupCaretaker,
+                questions: questions)
    }
-   
-   // MARK: - QuestionStrategy
-   public var title: String {
-      return questionGroup.title
-   }
-   
-   public func currentQuestion() -> Question {
-      return questionGroup.questions[questionIndex]
-   }
-   
-   public func advanceToNextQuestion() -> Bool {
-      guard questionIndex + 1 < questionGroup.questions.count else {
-         return false
-      }
-      questionIndex += 1
-      return true
-    }
-     
-    public func markQuestionCorrect(_ question: Question) {
-       correctCount += 1
-    }
-    
-    public func markQuestionIncorrect(_ question: Question) {
-       incorrectCount += 1
-    }
-    
-    public func questionIndexTitle() -> String {
-       return "\(questionIndex + 1)/" + "\(questionGroup.questions.count)"
-    }
 }
