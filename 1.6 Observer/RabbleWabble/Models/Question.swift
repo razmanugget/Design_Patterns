70a1999588dfb76c3e95387617c465e9f80ca351
Rami <rami_s@hotmail.com>
2020-02-24 13:49:59 +0100

    Add basic design patterns with markdown descriptions

--- 1.6 Observer/RabbleWabble/Models/Question.swift
+++ 1.6 Observer/RabbleWabble/Models/Question.swift
@@ -7,6 +7,23 @@
 //
 
 import Foundation
+import Combine
+
+public class User {
+   @Published var name: String
+   public init(name: String) {
+      self.name = name
+   }
+}
+
+let user = User(name: "Ray")
+let publisher = user.$name
+
+var subscriber: AnyCancellable? = publisher.sink() {
+   print("User's name is \($0)")
+}
+
+user.name = "Rami"
 
 public class Question: Codable {
    public let answer: String
