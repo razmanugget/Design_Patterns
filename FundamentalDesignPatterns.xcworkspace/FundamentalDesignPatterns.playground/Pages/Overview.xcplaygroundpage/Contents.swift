82211403a2ce615fdc0e5df9b02661ace77f488b
Rami <rami_s@hotmail.com>
2020-02-25 18:16:20 +0100

    Test Overview update

--- FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Overview.xcplaygroundpage/Contents.swift
+++ FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Overview.xcplaygroundpage/Contents.swift
@@ -4,18 +4,18 @@
  - - - - - - - - - -
  There are three types of design patterns:
  
- 1. **Structural patterns** describe how objects are composed to form larger subsystems.
+ 1. **Structural patterns** - how objects are composed to form larger subsystems.
  Examples of structural patterns are:
  - [Model-view-controller (MVC)](Model-View-Controller)
  
- 2. **Behavioral patterns** describe how objects communicate with each other.
+ 2. **Behavioral patterns** - how objects communicate with each other.
  Examples of behavioral patterns are:
  - [Delegation](Delegation)
  - [Strategy](Strategy)
  - [Observer](Observer)
  - [Memento](Memento)
  
- 3. **Creational patterns** instantiate or "create" objects for you.
+ 3. **Creational patterns** - instantiate or "create" objects for you.
  Examples of creational patterns are:
  - [Singleton](Singleton)
  - [Builder](Builder)
