8ce049d9480909e982e6651ae610971beb34edfe
Rami <rami_s@hotmail.com>
2020-02-25 13:37:24 +0100

    Add observer design pattern to QuestionGroup for correct/incorrectCount ch 8

--- 1.6 Observer/RabbleWabble/Models/QuestionGroup.swift
+++ 1.6 Observer/RabbleWabble/Models/QuestionGroup.swift
@@ -7,13 +7,42 @@
 //
 
 import Foundation
+import Combine
 
 public class QuestionGroup: Codable {
    
    public class Score: Codable {
-      public var correctCount: Int = 0
-      public var incorrectCount: Int = 0
+      public var correctCount: Int = 0 {
+         didSet { updateRunningPercentage() }
+      }
+      public var incorrectCount: Int = 0 {
+         didSet { updateRunningPercentage() }
+      }
+      
+      @Published public var runningPercentage: Double = 0
+      
       public init() { }
+      
+      private enum CodingKeys: String, CodingKey {
+         case correctCount
+         case incorrectCount
+      }
+      
+      public required init(from decoder: Decoder) throws {
+         let container = try decoder.container(keyedBy: CodingKeys.self)
+         self.correctCount = try container.decode(Int.self, forKey: .correctCount)
+         self.incorrectCount = try container.decode(Int.self, forKey: .incorrectCount)
+         updateRunningPercentage()
+      }
+      
+      private func updateRunningPercentage() {
+         let totalCount = correctCount + incorrectCount
+         guard totalCount > 0 else {
+            runningPercentage = 0
+            return
+         }
+         runningPercentage = Double(correctCount) / Double(totalCount)
+      }
    }
    
    public let questions: [Question]
